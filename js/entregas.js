6c34397c103644393d74d257442e7ed8ecbd18ad
Ítalo Sousa <italusousa@gmail.com>
2018-07-16 16:27:25 -0300

    Adicionada a página de entregas e o primeiro link para entrega

---
+++ js/entregas.js
@@ -0,0 +1,37 @@
+(function() {
+    $.getJSON( "dados/entregas.json", function( dados ) {
+        var htmlStr = [];
+        $.each( dados, function(e) {
+            documento = dados[e];
+            item = '';
+
+            if (e === 0) {
+                item += '<div class="row">';
+            } else if ((e % 3) === 0) {
+                item += '<div class="row mt-5">';
+            }
+
+            item += '<div class="col-lg-4">';
+            item +=     '<div class="card wow fadeIn">';
+            item +=         '<div class="card-body team-member">';
+            item +=             '<h5 class="card-title">'+ documento.titulo +'</h5>';
+            item +=             '<p class="card-text">'+ documento.descricao +'</p>';
+            item +=             '<p class="card-text"> <i>'+ documento.versao +'</i> </p>';
+            item +=             '<a class="social-link rounded-circle " data-toggle="tooltip" data-placement="bottom" title="Download" target="_blank" href="'+ documento.linkDoc +'">';
+            item +=                 '<i class="fa fa-download"></i>';
+            item +=             '</a>';
+            item +=         '</div>'
+            item +=     '</div>'    
+            item += '</div>';
+
+            if (e > 0 && (e % 3) === 2) {
+                item += '</div>';  
+            } else if (e === dados.length) {
+                item += '</div>'; 
+            }
+
+            htmlStr.push( item );
+        });
+        $('#documentos').append(htmlStr.join(""));
+      });
+})();
